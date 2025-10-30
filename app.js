09b89e7d2178c1799d275285bbabf6100d20c08a
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-10-30 16:25:53 +0800

    Add Env Vars

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('node:path');
 const categoryRouter = require('./routes/route')
+require('dotenv').config()
 
 const app = express();
 
@@ -8,7 +9,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use('/', categoryRouter)
 
-const PORT = 3000 
+const PORT = process.env.PORT || 3000 
 
 app.listen((PORT), (error) => {
     if (error) {
@@ -17,3 +18,4 @@ app.listen((PORT), (error) => {
 
     console.log('Server is listening at port 3000')
 })
+
