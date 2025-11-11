c1845eac039f687beb5a60405bad25a3a0e2f44e
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-11-11 09:59:38 +0800

    Create separate resourceController

---
+++ controllers/resourceController.js
@@ -0,0 +1,50 @@
+const db = require('../db/queries');
+
+async function getResourceDetail(req, res) {
+    const {categoryId, id} = req.params;
+
+    const resource = await db.getResourceDetail(id);
+    res.render("resourceDetail", {resource, categoryId});
+}
+
+async function createResourcePost(req, res) {
+    const resource = req.body;
+
+    const result = await db.insertResource(
+        resource.title, 
+        resource.description, 
+        resource.type,
+        resource.category_id
+    )
+    console.log(result);
+    res.redirect(`/categories/${resource.category_id}`)
+}
+
+async function updateResourceGet(req, res) {
+    const {categoryId, id} = req.params
+    const resource = await db.getResourceDetail(id)
+    
+    res.render('updateResource', {categoryId, resource})
+}
+
+
+async function updateResourcePost(req, res) {
+
+}
+
+async function deleteResource(req, res) {
+    const {categoryId, id} = req.params;
+
+    const result = await db.deleteResource(id);
+    console.log(result);
+
+    res.redirect(`/categories/${categoryId}`);
+}
+
+module.exports = {
+    getResourceDetail,
+    createResourcePost,
+    updateResourceGet,
+    updateResourcePost,
+    deleteResource,
+}
