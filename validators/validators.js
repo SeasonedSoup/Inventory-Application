e1925e9a7b16e1736bf92089a7d948bb848f7625
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-11-12 11:51:37 +0800

    Start Validation on category and resource
    just simple validation since its not too complicated

---
+++ validators/validators.js
@@ -0,0 +1,13 @@
+const {body} = require('express-validator');
+
+lengthErr = 'must be beetween 1 to 100 characters';
+
+//validating the category
+const validateCategory = [
+
+]
+
+//validating the resource
+const validateResource = [
+
+]
