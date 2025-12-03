58bfed3b13dcc68eb1cccb1ecf613039d8bd2142
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-12-03 19:50:34 +0800

    Final

--- db/pool.js
+++ db/pool.js
@@ -3,9 +3,5 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 module.exports = new Pool({
-    host: 'localhost',
-    user: process.env.DB_USER,
-    database: 'resource_notes',
-    password: process.env.DB_PASSWORD,
-    port: 5432,
+    connectionString: process.env.DB_INTERNAL_URL
 });
